30fb4d08f76bf47597779bf54c7838d352e3e85d
maxwarch <maximekuil@gmail.com>
2024-06-24 12:53:35 +0000

    clean code

--- max/api/main.py
+++ max/api/main.py
@@ -16,17 +16,6 @@ model = CamembertForSequenceClassification.from_pretrained(path)
 trainer = Trainer(model=model)
 
 
-text = """Après de multiples tournées à succès et plus de 2 millions de billets vendus, Messmer, connu et reconnu comme le Maître Mondial de l’Hypnose revient près de chez vous !
-
-Dans 13Hz, Messmer vous invite à entrer dans son mystérieux et hilarant univers où la frontière entre la réalité et l’illusion s’efface, pour diriger vos pensées vers des territoires inconnus.
-
-Avec sa présence charismatique inégalée et son talent exceptionnel, le recordman en hypnose collective avec 1066 personnes hypnotisées en moins de 5 minutes, vous plonge au cœur de vos pensées les plus profondes avant de vous guider à travers un jeu subtil d’ondes cérébrales à 13Hz.
-
-Le fascinateur vous entraîne vers un état de conscience unique où la volonté et le contrôle de nos vies prennent une nouvelle dimension.
-
-Osez découvrir l’expérience Messmer, où la maîtrise de soi et la fascination se rencontrent."""
-
-
 def sort_by_score(data):
     return sorted(data, key=lambda item: item["score"], reverse=True)
 
@@ -47,8 +36,3 @@ app = FastAPI()
 @app.post("/predict")
 def predict(text: str):
     return predict_fn(text)
-
-
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
