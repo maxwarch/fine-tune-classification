1055998a345e91b3c764180be49ecaaccf5ad7da
Maxime KUIL <maximekuil@gmail.com>
2024-06-24 12:42:33 +0200

    ajout score

--- streamlit/Recherche.py
+++ streamlit/Recherche.py
@@ -1,3 +1,4 @@
+from math import floor
 import streamlit as st
 import sqlite3
 import pandas as pd
@@ -7,10 +8,10 @@ from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode, DataReturnMode
 st.title("Recherche d'évènements")
 
 
-conn = sqlite3.connect('evenements.db')
+conn = sqlite3.connect("evenements.db")
 c = conn.cursor()
 
-c.execute('''
+c.execute("""
     CREATE TABLE IF NOT EXISTS evenements (
         id INTEGER PRIMARY KEY,
         url TEXT NOT NULL,
@@ -18,34 +19,125 @@ c.execute('''
         description TEXT NOT NULL,
         cat1 TEXT,
         cat2 TEXT,
-        cat3 TEXT
+        cat3 TEXT,
+        score1 REAL,
+        score2 REAL,
+        score3 REAL
     )
-''')
+""")
 
-c.execute('''
+
+c.execute("""
     CREATE TABLE IF NOT EXISTS ip (
         ip TEXT NOT NULL
     )
-''')
-c.execute('SELECT count(1) nb FROM ip')
+""")
+c.execute("SELECT count(1) nb FROM ip")
 row = c.fetchone()
 if row[0] != 1:
-    c.execute('DELETE FROM ip')
+    c.execute("DELETE FROM ip")
     c.execute('INSERT INTO ip VALUES ("0.0.0.0")')
     conn.commit()
 
 
-cat1 = st.sidebar.selectbox("Catégorie 1", ('', 'Action','Art','Atelier','Balade','Brocante','Concert','Conférence','Culture','Danse','Détente','Environnement','Exposition','Famille','Festival','Fête','Gastronomie','Histoire','Jeu','Marché','Santé','Spectacle','Sport','Théatre','Visite'))
-cat2 = st.sidebar.selectbox("Catégorie 2", ('', 'Action','Art','Atelier','Balade','Brocante','Concert','Conférence','Culture','Danse','Détente','Environnement','Exposition','Famille','Festival','Fête','Gastronomie','Histoire','Jeu','Marché','Santé','Spectacle','Sport','Théatre','Visite'))
-cat3 = st.sidebar.selectbox("Catégorie 3", ('', 'Action','Art','Atelier','Balade','Brocante','Concert','Conférence','Culture','Danse','Détente','Environnement','Exposition','Famille','Festival','Fête','Gastronomie','Histoire','Jeu','Marché','Santé','Spectacle','Sport','Théatre','Visite'))
+cat1 = st.sidebar.selectbox(
+    "Catégorie 1",
+    (
+        "",
+        "Action",
+        "Art",
+        "Atelier",
+        "Balade",
+        "Brocante",
+        "Concert",
+        "Conférence",
+        "Culture",
+        "Danse",
+        "Détente",
+        "Environnement",
+        "Exposition",
+        "Famille",
+        "Festival",
+        "Fête",
+        "Gastronomie",
+        "Histoire",
+        "Jeu",
+        "Marché",
+        "Santé",
+        "Spectacle",
+        "Sport",
+        "Théatre",
+        "Visite",
+    ),
+)
+cat2 = st.sidebar.selectbox(
+    "Catégorie 2",
+    (
+        "",
+        "Action",
+        "Art",
+        "Atelier",
+        "Balade",
+        "Brocante",
+        "Concert",
+        "Conférence",
+        "Culture",
+        "Danse",
+        "Détente",
+        "Environnement",
+        "Exposition",
+        "Famille",
+        "Festival",
+        "Fête",
+        "Gastronomie",
+        "Histoire",
+        "Jeu",
+        "Marché",
+        "Santé",
+        "Spectacle",
+        "Sport",
+        "Théatre",
+        "Visite",
+    ),
+)
+cat3 = st.sidebar.selectbox(
+    "Catégorie 3",
+    (
+        "",
+        "Action",
+        "Art",
+        "Atelier",
+        "Balade",
+        "Brocante",
+        "Concert",
+        "Conférence",
+        "Culture",
+        "Danse",
+        "Détente",
+        "Environnement",
+        "Exposition",
+        "Famille",
+        "Festival",
+        "Fête",
+        "Gastronomie",
+        "Histoire",
+        "Jeu",
+        "Marché",
+        "Santé",
+        "Spectacle",
+        "Sport",
+        "Théatre",
+        "Visite",
+    ),
+)
 
-request = f'''
+request = f"""
 SELECT *
   FROM evenements
  WHERE ("{cat1}" = "" OR cat1 = "{cat1}" OR cat2 = "{cat1}" OR cat3 = "{cat1}")
    AND ("{cat2}" = "" OR cat1 = "{cat2}" OR cat2 = "{cat2}" OR cat3 = "{cat2}")
    AND ("{cat3}" = "" OR cat1 = "{cat3}" OR cat2 = "{cat3}" OR cat3 = "{cat3}")
-'''
+"""
 print(request)
 
 c.execute(request)
@@ -62,12 +154,33 @@ with col1:
     #     st.write(f'{row[0]} : {row[1]} {row[2]} {row[3]} {row[4]} {row[5]} {row[6]}')
 
     # Convertir les résultats en DataFrame
-    df = pd.DataFrame(rows, columns=['ID', 'URL', 'Titre', 'Description', 'Catégorie 1', 'Catégorie 2', 'Catégorie 3'])
+    df = pd.DataFrame(
+        rows,
+        columns=[
+            "ID",
+            "URL",
+            "Titre",
+            "Description",
+            "Catégorie 1",
+            "Catégorie 2",
+            "Catégorie 3",
+            "score1",
+            "score2",
+            "score3",
+        ],
+    )
 
     for row in rows:
-        st.write(f"{row[2]} [{row[4]}|{row[5]}|{row[6]}]")
+        st.html(
+            f"""{row[2]} 
+                    <ul style="padding-left:30px">
+                        <li>{row[4]}: {floor(row[7] * 100)}%</li>
+                        <Li>{row[5]}: {floor(row[8] * 100)}%</li>
+                        <li>{row[6]}: {floor(row[9] * 100)}%</li>
+                    </ul>"""
+        )
         buttons.append(st.button("Afficher", key=row[0]))
-    
+
 
 with col2:
     st.header("Informations")
--- streamlit/pages/AI - MAJ catégories.py
+++ streamlit/pages/AI - MAJ catégories.py
@@ -4,18 +4,18 @@ import streamlit as st
 import sqlite3
 import requests
 import urllib
-from  dotenv import load_dotenv
+from dotenv import load_dotenv
 import os
 
 
-ENV = '.env'
+ENV = ".env"
 BASE_DIR = Path(__file__).absolute()
 load_dotenv(os.path.join(BASE_DIR.parent.parent, ENV))
-cle = os.environ.get('CLE')
+cle = os.environ.get("CLE")
 
 st.title("IA - Mise à jour des catégories")
 
-conn = sqlite3.connect('evenements.db')
+conn = sqlite3.connect("evenements.db")
 c = conn.cursor()
 
 
@@ -27,20 +27,23 @@ c = conn.cursor()
 
 # récupération de l'IP
 headers = {
-    'Accept': 'application/vnd.github+json',
-    'Authorization': f'Bearer {cle}',
-    'X-GitHub-Api-Version': '2022-11-28',
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"Bearer {cle}",
+    "X-GitHub-Api-Version": "2022-11-28",
 }
 
-response = requests.get('https://api.github.com/repos/maxwarch/fine-tune-classification/actions/variables', headers=headers)
+response = requests.get(
+    "https://api.github.com/repos/maxwarch/fine-tune-classification/actions/variables",
+    headers=headers,
+)
 if response.status_code == 200:
     events = response.json()
-    ip = events['variables'][0]['value']
-    print(">>>>>>>>>>IP>>>>>",ip)
+    ip = events["variables"][0]["value"]
+    print(">>>>>>>>>>IP>>>>>", ip)
 
 api_url = f"http://{ip}:8001/predict?text="
-if st.button('mise à jour des catégories'):
-    c.execute('SELECT id, description FROM evenements WHERE cat1 IS NULL')
+if st.button("mise à jour des catégories"):
+    c.execute("SELECT id, description FROM evenements WHERE cat1 IS NULL")
     rows = c.fetchall()
 
     for row in rows:
@@ -48,41 +51,48 @@ if st.button('mise à jour des catégories'):
         response = requests.post(url)
         if response.status_code == 200:
             events = response.json()
-            
-            cat = [0,1,2]
+
+            cat = [0, 1, 2]
+            sco = [0, 1, 2]
             for index in range(3):
-                label = events[index]['label']
-                score = events[index]['score']
+                label = events[index]["label"]
+                score = events[index]["score"]
+
                 if score >= 0.5:
                     cat[index] = f"'{label}'"
+                    sco[index] = f"'{score}'"
                 else:
-                    cat[index] = 'NULL'
-                c.execute(f'''
+                    cat[index] = "NULL"
+                    sco[index] = -1
+                c.execute(f"""
                            UPDATE evenements
                               SET cat1 = {cat[0]},
                                   cat2 = {cat[1]},
-                                  cat3 = {cat[2]}
+                                  cat3 = {cat[2]},
+                                  score1 = {sco[0]},
+                                  score2 = {sco[1]},
+                                  score3 = {sco[2]}
                                  WHERE id = {row[0]}
-                          ''')
+                          """)
                 conn.commit()
 
         else:
             st.error("Erreur lors de la récupération des événements")
             events = []
 
-    st.write('mise à jour effectuées')
+    st.write("mise à jour effectuées")
 
 
 # Afficher toutes les valeurs de texte dans la table
-c.execute('''
+c.execute("""
           SELECT id, title, cat1, cat2, cat3
             FROM evenements
            WHERE cat1 IS NULL
            ORDER BY id DESC
-          ''')
+          """)
 rows = c.fetchall()
 
 for row in rows:
-    st.write(f'{row[0]} > {row[1]} : {row[2]} | {row[3]} | {row[4]}')
+    st.write(f"{row[0]} > {row[1]} : {row[2]} | {row[3]} | {row[4]}")
 
 conn.close()
